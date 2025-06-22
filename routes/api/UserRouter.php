b417c4ed714b624225d05d4a135e0b273d21c69c
pauloprogg <almeida.desenvolve@gmail.com>
2025-06-22 18:00:24 -0300

    fix: changing routes prefix

--- routes/api/UserRouter.php
+++ routes/api/UserRouter.php
@@ -5,9 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
 
-
 Route::prefix('user')->group(function () {
-    Route::get('/users-by-client', [UserController::class,'getUsersByClientId'])->middleware(['auth', 'access.level:3']);
-    Route::post('/users-add', [UserController::class,'store'])->middleware(['auth', 'access.level:3']);
+    Route::get('/get-users', [UserController::class,'getUsersByClientId'])->middleware(['auth', 'access.level:3']);
+    Route::post('/add-user', [UserController::class,'store'])->middleware(['auth', 'access.level:3']);
 });
 
