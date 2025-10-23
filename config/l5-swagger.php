8c7c24b146200e503292f24280cd600a113f71e4
pauloprogg <almeida.desenvolve@gmail.com>
2025-10-23 19:19:37 -0300

    feat: adding dynamic prefix for swagger documentation url

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -12,7 +12,7 @@ return [
                 /*
                  * Route for accessing api documentation interface
                  */
-                'api' => 'api/documentation',
+                'api' => 'api/'.env('APP_VERSION','v1').'/documentation',
             ],
             'paths' => [
                 /*
